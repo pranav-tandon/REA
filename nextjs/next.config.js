63bd63291c6d3237d0e5737d5f44b56cdf057234
gauransh <gauranshtandon@gmail.com>
2025-02-06 07:10:30 -0800

    STRY: Add docker-compose support

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // Add this to allow connections from any host
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://backend:8005/api/:path*',
+        },
+      ];
+    },
   };
   
   module.exports = nextConfig;
